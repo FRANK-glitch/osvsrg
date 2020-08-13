2aca3135630c5269c5c4da74b5f92e71b190909b
REGENER ATE <stirmanjr@gmail.coom>
2020-08-12 20:06:12 -0400

    Added first two c/s

---
+++ src/Client/Controllers/MainController.lua
@@ -0,0 +1,19 @@
+-- Main Controller
+-- Username
+-- August 12, 2020
+
+
+
+local MainController: AeroController = {}
+
+function MainController:Start()
+	print("Started controller")
+end
+
+
+function MainController:Init()
+	
+end
+
+
+return MainController
---
+++ src/Server/Services/TestService.lua
@@ -0,0 +1,19 @@
+-- Test Service
+-- Username
+-- August 12, 2020
+
+
+
+local TestService: AeroService = {Client = {}}
+
+function TestService:Start()
+    
+end
+
+
+function TestService:Init()
+	
+end
+
+
+return TestService
